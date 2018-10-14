751e9e6a37ae82ca9b4610af48169ba59a3afc40
DESKTOP-ISRPCIK\Kria <krianoto@gmail.com>
2018-10-15 04:16:27 +1030

    Attempt at Merging - Chunk 2

---
+++ The Beyond/Assets/UI/Compass.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Compass : MonoBehaviour {
+
+    public Transform Player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles = -Vector3.forward * Player.eulerAngles.y;
+    }
+
+}
