d2db2fbcd7e9143bf7574321a67f6fc9ced99339
DESKTOP-ISRPCIK\Kria <krianoto@gmail.com>
2018-10-16 23:09:35 +1030

    Added Main Scene
    Only two issues: Peasant Farmer and Merchant didn't import correctly and my attempts at adding them don't seem to work. There's a .max file that Unity recognizes in the Beyond 10.10 project, but isn't recognized in this project.

---
+++ The Beyond/Assets/Scenes/Main Scene/MENU SCENE/MenuScript/ButtonLoadScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonLoadScene : MonoBehaviour {
+
+    public string _sceneName = string.Empty;
+    public void OnButtonPressed()
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+}
+		
+	
---
+++ The Beyond/Assets/Scenes/Main Scene/MENU SCENE/MenuScript/ButtonQuitApplication.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonQuitApplication : MonoBehaviour {
+
+    public void OnButtonQuit()
+    {
+        Application.Quit();
+
+    }
+}
